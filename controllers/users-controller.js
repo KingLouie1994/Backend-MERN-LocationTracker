f09264625d351cea55d6a6c570352a5dd7568820
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-05 14:35:05 +0100

    created controller to signup user

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -15,7 +15,18 @@ const getUsers = (req, res, next) => {
   res.status(200).json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {};
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+  res.status(201).json({ user: createdUser });
+};
 
 const login = (req, res, next) => {};
 
