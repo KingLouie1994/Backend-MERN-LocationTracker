4bcd3ce8cbc3cc7049749c94513a30e4b807c33d
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-05 14:44:42 +0100

    signup with email that already exists not longer possible

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -17,6 +17,13 @@ const getUsers = (req, res, next) => {
 
 const signup = (req, res, next) => {
   const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (hasUser) {
+    throw new HttpError("Could not create user! E-Mail already exists", 422);
+  }
+
   const createdUser = {
     id: uuid(),
     name,
