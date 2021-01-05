eeccbfd4c8124a51c61a26a80192415364cf50af
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-05 14:19:13 +0100

    Created a route and controller to delete a place by id

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -2,7 +2,7 @@ const { v4: uuid } = require("uuid");
 
 const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
@@ -70,7 +70,11 @@ const updatePlace = (req, res, next) => {
   res.status(200).json({ place: updatedPlace });
 };
 
-const deletePlace = (req, res, next) => {};
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  res.status(200).json({ message: "Deleted place!" });
+};
 
 exports.getPlaceByID = getPlaceByID;
 exports.getPlaceByUserId = getPlaceByUserId;
