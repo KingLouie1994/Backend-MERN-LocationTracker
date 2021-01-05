2e39fde41157aae7fc93b26334714f02b82fb9d6
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-05 16:04:36 +0100

    wrote a function that makes a get request to the google maps api to receive coordinates of given address

---
+++ util/location.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+
+const API_KEY = "AIzaSyAzMixUC5-3JT56tsNPI5XhOwnlo9btKi8";
+
+async function getCoordsForAdress(address) {
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for this address.",
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+  return coordinates;
+}
+
+module.exports = getCoordsForAdress;
