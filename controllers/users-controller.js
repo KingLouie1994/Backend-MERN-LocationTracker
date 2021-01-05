8c597985538c082cc95b3c299947012c6f210744
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-05 14:33:09 +0100

    created controller to get all users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,14 +1,23 @@
+const { v4: uuid } = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Luis Schekerka",
+    email: "test@test.com",
+    password: "test1234",
+  },
+];
+
 const getUsers = (req, res, next) => {
+  res.status(200).json({ users: DUMMY_USERS });
+};
 
-}
+const signup = (req, res, next) => {};
 
-const signup = (req, res, next) => {
-
-}
-
-const login = (req, res, next) => {
-
-}
+const login = (req, res, next) => {};
 
 exports.getUsers = getUsers;
 exports.signup = signup;
