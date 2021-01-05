9a2032c9cc1df484a66300f908ddc41c994a234f
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-05 14:30:25 +0100

    prepared files to create user routes and controllers

---
+++ controllers/users-controller.js
@@ -0,0 +1,15 @@
+const getUsers = (req, res, next) => {
+
+}
+
+const signup = (req, res, next) => {
+
+}
+
+const login = (req, res, next) => {
+
+}
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
