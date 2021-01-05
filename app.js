7e489fc4defea7edf8a7cb15353c6a093996be58
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-05 10:45:35 +0100

    created the first real route with dummy data to receive place by place id

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const placesRoutes = require("./routes/places-routes");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Location Tracker API");
+app.use("/api/places", placesRoutes);
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Welcome to the Location Tracker API</>");
 });
 
 app.listen(5000);
