139e1c25fc86fd9a6fc9752eb56f3211f9371137
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-07 11:16:55 +0100

    Rebuilt update place by user id controller that it works with the mongo db

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -64,11 +64,9 @@ const getPlacesByUserId = async (req, res, next) => {
     );
     return next(error);
   } else {
-    res
-      .status(200)
-      .json({
-        places: places.map((place) => place.toObject({ getters: true })),
-      });
+    res.status(200).json({
+      places: places.map((place) => place.toObject({ getters: true })),
+    });
   }
 };
 
@@ -115,7 +113,7 @@ const createPlace = async (req, res, next) => {
   }
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid input passed, please check it", 422);
@@ -124,14 +122,31 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  let updatedPlace;
+  try {
+    updatedPlace = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      `Updating place failed, please try again.`,
+      500
+    );
+    return next(error);
+  }
+
   updatedPlace.title = title;
   updatedPlace.description = description;
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+  try {
+    await updatedPlace.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wring, could not update place",
+      500
+    );
+    return next(error);
+  }
 
-  res.status(200).json({ place: updatedPlace });
+  res.status(200).json({ place: updatedPlace.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
