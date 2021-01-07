a27cde4c4c910fc3368bfbc77a50c37610361e9f
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-07 13:57:53 +0100

    creating a place now contains the creators id and the place gets pushed to users places array

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,10 +1,11 @@
-const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 
 const HttpError = require("../models/http-error");
-const getCoordsForAdress = require("../util/location");
+const getCoordsForAddress = require("../util/location");
 
 const Place = require("../models/place");
+const User = require("../models/user");
 
 const getPlaceByID = async (req, res, next) => {
   const placeId = req.params.pid;
@@ -59,14 +60,16 @@ const getPlacesByUserId = async (req, res, next) => {
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(new HttpError("Invalid input passed, please check it", 422));
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
 
   const { title, description, address, creator } = req.body;
 
   let coordinates;
   try {
-    coordinates = await getCoordsForAdress(address);
+    coordinates = await getCoordsForAddress(address);
   } catch (error) {
     return next(error);
   }
@@ -81,22 +84,40 @@ const createPlace = async (req, res, next) => {
     creator,
   });
 
+  let user;
   try {
-    await createdPlace
-      .save()
-      .then(() => {
-        res.status(201).json({ place: createdPlace });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    user = await User.findById(creator);
   } catch (err) {
     const error = new HttpError(
-      `Creating place failed, please try again.`,
+      "Creating place failed, please try again.",
       500
     );
     return next(error);
   }
+
+  if (!user) {
+    const error = new HttpError("Could not find user for provided id", 404);
+    return next(error);
+  }
+
+  console.log(user);
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdPlace.save({ session: sess });
+    user.places.push(createdPlace);
+    await user.save({ session: sess });
+    await sess.commitTransaction();
+  } catch (err) {
+    const error = new HttpError(
+      "Creating place failed, please try again.",
+      500
+    );
+    return next(error);
+  }
+
+  res.status(201).json({ place: createdPlace });
 };
 
 const updatePlace = async (req, res, next) => {
