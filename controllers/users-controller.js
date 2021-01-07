aba1d982e7c2679c122f27d1d605fe4240274e36
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-07 12:53:47 +0100

    get users works with mongodb now

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -5,8 +5,18 @@ const HttpError = require("../models/http-error");
 
 const User = require("../models/user");
 
-const getUsers = (req, res, next) => {
-  res.status(200).json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching users failed, please try again later",
+      500
+    );
+    return next(error);
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
