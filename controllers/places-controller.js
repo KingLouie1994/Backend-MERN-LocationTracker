b8c2b35e70df8bc7ed21f4de8bb9e76ba8a42e5d
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-07 11:22:07 +0100

    Rebuilt delete place by user id controller that it works with the mongo db

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -149,12 +149,30 @@ const updatePlace = async (req, res, next) => {
   res.status(200).json({ place: updatedPlace.toObject({ getters: true }) });
 };
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
-  if (DUMMY_PLACES.find((p) => p.id === placeId)) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
+
+  let deletedPlace;
+  try {
+    deletedPlace = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not delete place.",
+      500
+    );
+    return next(error);
   }
-  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+
+  try {
+    await deletedPlace.remove();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not delete place.",
+      500
+    );
+    return next(error);
+  }
+
   res.status(200).json({ message: "Deleted place!" });
 };
 
