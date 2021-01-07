a643b6a917f3ad737e94b535bb397f4dc3580a2e
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-07 12:45:52 +0100

    login user works now with mongo db

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -5,7 +5,6 @@ const HttpError = require("../models/http-error");
 
 const User = require("../models/user");
 
-
 const getUsers = (req, res, next) => {
   res.status(200).json({ users: DUMMY_USERS });
 };
@@ -55,15 +54,29 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("Could not identify user!", 401);
-  } else {
-    res.json({ message: "Logged in!" });
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
   }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in",
+      401
+    );
+    return next(error);
+  }
+
+  res.json({ message: "Logged in!" });
 };
 
 exports.getUsers = getUsers;
