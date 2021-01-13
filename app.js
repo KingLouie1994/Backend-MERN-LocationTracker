8bb224294e56336fb9190d8b1bfbecb028204a6a
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-01-13 14:01:04 +0100

    minor changes

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ mongoose
   )
   .then(() => {
     app.listen(8000);
+    console.log("Api is running on port 8000");
   })
   .catch((err) => {
     console.log(err);
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -85,7 +85,10 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  res.json({ message: "Logged in!" });
+  res.json({
+    message: "Logged in!",
+    user: existingUser.toObject({ getters: true }),
+  });
 };
 
 exports.getUsers = getUsers;
