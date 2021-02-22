5ceeabf4ef7f800a015ae43a4b93603185654f74
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-22 12:42:14 +0100

    installed dotenv to handle environment variables

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json());
 
