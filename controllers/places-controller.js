fc91b06fcdad91cef5937216ac41e2aa3590e606
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-22 12:07:46 +0100

    preparation to deploy

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -102,8 +102,6 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-  console.log(user);
-
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
